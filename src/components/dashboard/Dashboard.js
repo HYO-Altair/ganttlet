568cb42cfbd4c4d00dd23935384dec2725dcd52b
Nick Chen <nickchen@usc.edu>
2020-07-07 21:08:09 -0500

    surgery

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -123,92 +123,51 @@ export default function Dashboard() {
     };
 
     const Proj = [
-            {
-                name: "Project1",
-                description: "Describe Project1"
-            },
+        {
+            name: 'Project1',
+            description: 'Describe Project1',
+        },
 
-            {
-                name: "Project2",
-                description: "Describe Project2"
-            },
+        {
+            name: 'Project2',
+            description: 'Describe Project2',
+        },
 
-            {
-                name: "Project3",
-                description: "Describe Project3"
-            },
-            {
-                name: "Project4",
-                description: "Describe Project4"
-            },
-            {
-                name: "Project5",
-                description: "Describe Project5"
-            }
-    ]
+        {
+            name: 'Project3',
+            description: 'Describe Project3',
+        },
+        {
+            name: 'Project4',
+            description: 'Describe Project4',
+        },
+        {
+            name: 'Project5',
+            description: 'Describe Project5',
+        },
+    ];
 
     return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-                <Toolbar className={classes.toolbar}>
-                    <IconButton
-                        edge="start"
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-                        Dashboard
-                    </Typography>
-                    <IconButton color="inherit">
-                        <Badge badgeContent={4} color="secondary">
-                            <NotificationsIcon />
-                        </Badge>
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                variant="permanent"
-                classes={{
-                    paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-                }}
-                open={open}
-            >
-                <div className={classes.toolbarIcon}>
-                    <IconButton onClick={handleDrawerClose}>
-                        <ChevronLeftIcon />
-                    </IconButton>
-                </div>
-                <Divider />
-                <List>{mainListItems}</List>
-                <Divider />
-                <List>{secondaryListItems}</List>
-            </Drawer>
-            <main className={classes.content}>
-                <div className={classes.appBarSpacer} />
-                <Container maxWidth="lg" className={classes.container}>
-                    <Grid container spacing={3}>
-                        {/* Schedule Table */}
-                        <Grid item xs={12}>
-                            <Paper className={classes.paper}>
-                                <Table />
-                            </Paper>
-                        </Grid>
-                        {Proj.map(p => (
-                            <Grid item xs={3}>
-                                <Projects Projects = {p} />
-                            </Grid>
-                        ))}
+        <main className={classes.content}>
+            <div className={classes.appBarSpacer} />
+            <Container maxWidth="lg" className={classes.container}>
+                <Grid container spacing={3}>
+                    {/* Schedule Table */}
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper}>
+                            <Table />
+                        </Paper>
                     </Grid>
-                    <Box pt={4}>
-                        <Copyright />
-                    </Box>
-                </Container>
-            </main>
-        </div>
+                    {Proj.map((p) => (
+                        <Grid item xs={3}>
+                            <Projects Projects={p} />
+                        </Grid>
+                    ))}
+                </Grid>
+                <Box pt={4}>
+                    <Copyright />
+                </Box>
+            </Container>
+        </main>
     );
 }

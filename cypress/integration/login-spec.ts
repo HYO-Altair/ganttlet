a414b6b45dd02482425eb8aa7d2d58971ed6174f
JosephPallipadan <pallipadanjoseph@gmail.com>
2020-07-10 14:31:09 +0400

    Removed the asinine should('exist') stuff from previous commit

--- cypress/integration/login-spec.ts
+++ cypress/integration/login-spec.ts
@@ -3,13 +3,13 @@ describe('Login Page Log in and Log Out Test', () => {
         cy.visit('http://localhost:3000/login');
         cy.wait(100);
 
-        cy.get('[data-cy=email]').should('exist').type('bot@cypress.com');
-        cy.get('[data-cy=password]').should('exist').type('123456abcdef');
-        cy.get('[data-cy=submitButton]').should('exist').click();
+        cy.get('[data-cy=email]').type('bot@cypress.com');
+        cy.get('[data-cy=password]').type('123456abcdef');
+        cy.get('[data-cy=submitButton]').click();
 
         cy.url().should('include', 'dashboard');
 
-        cy.get('[data-cy=logoutButton]').should('exist').click();
+        cy.get('[data-cy=logoutButton]').click();
 
         cy.url().should('include', 'login');
     });
@@ -20,9 +20,9 @@ describe('Invalid Login Attempt Tests', () => {
         cy.visit('http://localhost:3000/login');
         cy.wait(100);
 
-        cy.get('[data-cy=email]').should('exist').type('bot@cypress.com');
-        cy.get('[data-cy=password]').should('exist').type('Capitalism Is The Root Of All Evil!');
-        cy.get('[data-cy=submitButton]').should('exist').click();
+        cy.get('[data-cy=email]').type('bot@cypress.com');
+        cy.get('[data-cy=password]').type('Capitalism Is The Root Of All Evil!');
+        cy.get('[data-cy=submitButton]').click();
     });
 
     it('Should Not Accept Invalid Details', () => {
@@ -35,7 +35,7 @@ describe('Login Page Required Field Tests', () => {
         cy.visit('http://localhost:3000/login');
         cy.wait(100);
 
-        cy.get('[data-cy=submitButton]').should('exist').click();
+        cy.get('[data-cy=submitButton]').click();
     });
 
     it('Should Not Accept Empty Email', () => {
@@ -51,13 +51,9 @@ describe('Login Page Max/Min Length Tests', () => {
         cy.visit('http://localhost:3000/login');
         cy.wait(100);
 
-        cy.get('[data-cy=email]')
-            .should('exist')
-            .type(`a@${new Array(251 + 1).join('a')}.com`);
-        cy.get('[data-cy=password]')
-            .should('exist')
-            .type(new Array(11 + 1).join('a'));
-        cy.get('[data-cy=submitButton]').should('exist').click();
+        cy.get('[data-cy=email]').type(`a@${new Array(251 + 1).join('a')}.com`);
+        cy.get('[data-cy=password]').type(new Array(11 + 1).join('a'));
+        cy.get('[data-cy=submitButton]').click();
     });
 
     it('Should Not Accept Email Longer Than 256 Characters ', () => {
--- cypress/integration/register-spec.ts
+++ cypress/integration/register-spec.ts
@@ -3,15 +3,15 @@ describe('Register Page User Creation, Redirect and Deletion Test', () => {
         cy.visit('http://localhost:3000/register');
         cy.wait(100);
 
-        cy.get('[data-cy=firstName]').should('exist').type('Karl');
-        cy.get('[data-cy=lastName]').should('exist').type('Marx');
-        cy.get('[data-cy=email]').should('exist').type('marx@communism.com');
-        cy.get('[data-cy=password]').should('exist').type('123456abcdef');
-        cy.get('[data-cy=submitButton]').should('exist').click();
+        cy.get('[data-cy=firstName]').type('Karl');
+        cy.get('[data-cy=lastName]').type('Marx');
+        cy.get('[data-cy=email]').type('marx@communism.com');
+        cy.get('[data-cy=password]').type('123456abcdef');
+        cy.get('[data-cy=submitButton]').click();
         cy.url().should('include', 'dashboard');
 
-        cy.get('[data-cy=profileButton]').should('exist').click();
-        cy.get('[data-cy=deleteButton]').should('exist').click();
+        cy.get('[data-cy=profileButton]').click();
+        cy.get('[data-cy=deleteButton]').click();
         cy.url().should('include', 'login');
     });
 });
@@ -21,8 +21,8 @@ describe('Duplicate User Test', () => {
         cy.visit('http://localhost:3000/register');
         cy.wait(100);
 
-        cy.get('[data-cy=email]').should('exist').type('bot@cypress.com');
-        cy.get('[data-cy=submitButton]').should('exist').click();
+        cy.get('[data-cy=email]').type('bot@cypress.com');
+        cy.get('[data-cy=submitButton]').click();
         cy.get('[data-cy=emailContainer]').contains('Account Exists');
     });
 });
@@ -37,7 +37,7 @@ describe('Register Page Required Field Tests', () => {
         // Somehow, just a 1ms wait is sufficient here but this varies. My guess is it's a problem with the new version of
         //Cypress since no real changes have been made to the Register page or the test spec.
         cy.wait(100);
-        cy.get('[data-cy=submitButton]').should('exist').click();
+        cy.get('[data-cy=submitButton]').click();
     });
 
     it('Should Not Accept Empty First Name', () => {
@@ -59,19 +59,11 @@ describe('Register Page Max/Min Length Tests', () => {
         cy.visit('http://localhost:3000/register');
         cy.wait(100);
 
-        cy.get('[data-cy=firstName]')
-            .should('exist')
-            .type(new Array(65 + 1).join('a'));
-        cy.get('[data-cy=lastName]')
-            .should('exist')
-            .type(new Array(65 + 1).join('a'));
-        // cy.get('[data-cy=email]')
-        //     .should('exist')
-        //     .type(`a@${new Array(251 + 1).join('a')}.com`);
-        cy.get('[data-cy=password]')
-            .should('exist')
-            .type(new Array(11 + 1).join('a'));
-        cy.get('[data-cy=submitButton]').should('exist').click();
+        cy.get('[data-cy=firstName]').type(new Array(65 + 1).join('a'));
+        cy.get('[data-cy=lastName]').type(new Array(65 + 1).join('a'));
+        cy.get('[data-cy=email]').type(`a@${new Array(251 + 1).join('a')}.com`);
+        cy.get('[data-cy=password]').type(new Array(11 + 1).join('a'));
+        cy.get('[data-cy=submitButton]').click();
     });
 
     it('Should Not Accept First Name Longer Than 64 Characters ', () => {
@@ -80,9 +72,9 @@ describe('Register Page Max/Min Length Tests', () => {
     it('Should Not Accept Last Name Longer Than 64 Characters ', () => {
         cy.get('[data-cy=lastNameContainer]').contains('Too Long');
     });
-    // it('Should Not Accept Email Longer Than 256 Characters ', () => {
-    //     cy.get('[data-cy=emailContainer]').contains('Too Long');
-    // });
+    it('Should Not Accept Email Longer Than 256 Characters ', () => {
+        cy.get('[data-cy=emailContainer]').contains('Too Long');
+    });
     it('Should Not Accept Password Shorter Than 12 Characters ', () => {
         cy.get('[data-cy=passwordContainer]').contains('Too Short');
     });

85287e24eae3c845855696ccb009b94190b52232
Nick Chen <nickchen@usc.edu>
2020-07-14 10:58:04 -0500

    fixing unused vars that cause build to fail

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import { getFirebase } from 'react-redux-firebase';
 
 import { config } from '../config/firebase/firebase_consts';
 import firebase from 'firebase/app';
@@ -15,9 +15,6 @@ const initialState = {};
 const middleware = [thunk.withExtraArgument({ getFirebase })];
 
 const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
-
-// react-redux-firebase config
-const rrfConfig = {};
 
 const rrfProps = {
     firebase,
